9db8b1f81966b200ab5c3f70bca4f8e235659e9f
keerthana <keerthanacs1012@gmail.com>
2024-08-26 13:21:51 +0530

    Add files via upload

---
+++ ReachInboxAssignment/src/components/AppBar.tsx
@@ -0,0 +1,11 @@
+function AppBar() {
+  return (
+    <div className="border-[#25262B] bg-black border-b-2 fixed text-white h-16 w-screen flex items-center justify-center">
+      <div className="">
+        <h1>ReachIbox</h1>
+      </div>
+    </div>
+  );
+}
+
+export default AppBar;
